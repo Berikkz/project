60aa1fac2d6102f198e9a92fe93ad38746138a44
Berikkz <zhanibekuly.bkz@gmail.com>
2025-06-20 08:22:05 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,7 @@
-web: gunicorn --worker-class eventlet -w 1 wsgi:app --bind 0.0.0.0:$PORT
+
+import os
+from bot.py import app, main  # Замените your_bot_file на имя вашего файла
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 10000))
+    main()  # Запуск вебхука бота
