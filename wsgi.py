41b8e2bf1eca6624ecbf79df1cb96f42cda16cc3
Berikkz <zhanibekuly.bkz@gmail.com>
2025-06-20 08:42:11 +0500

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-
-import os
-from bot import app, main  # Замените your_bot_file на имя вашего файла
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    main()  # Запуск вебхука бота
